9f60e16603b361aa1053e0cae16dcbac936f04a0
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-11 21:00:44 +0200

    update router

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const server = http.createServer(app);
 const io = socketio(server); 
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection.')
+    console.log('We have a new connection.');
+
+    socket.on('disconnect', () => {
+        console.log('User have left.');
+    })
+
 })
 
 app.use(router);
