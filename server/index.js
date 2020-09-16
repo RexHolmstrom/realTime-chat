bf6f96d3312de8b6d2584683bdf0145d4141e6be
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-16 18:24:40 +0200

    index update

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ io.on("connection", (socket) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
+
+    socket.join(user.room);
   });
 
   socket.on("disconnect", () => {
