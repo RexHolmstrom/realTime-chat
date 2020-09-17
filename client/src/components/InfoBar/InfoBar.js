9912bf749302025ead7554a20777721a07cdd0b0
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-17 12:59:29 +0200

    infobar

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./InfoBar.css";
+
+const InfoBar = () => {
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online image" />
+    </div>
+  </div>;
+};
