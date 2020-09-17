dd2259f01720f384c8da1833fbbd9945d25fba1f
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-17 14:42:03 +0200

    room update

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./InfoBar.css";
 
-const InfoBar = () => {
+import closeIcon from "../../Icons/closeIcon";
+import onlineIcon from "../../Icons/onlineIcon";
+
+const InfoBar = ({ room }) => {
   <div className="infoBar">
     <div className="leftInnerContainer">
       <img className="onlineIcon" src={onlineIcon} alt="online image" />
-      <h3>roomName</h3>
+      <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
       <a href="/">
