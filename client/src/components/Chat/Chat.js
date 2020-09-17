d8140e555774e7a85a3ebb771c451612371355b9
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-17 12:06:58 +0200

    update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect }from 'react';
-import queryString from 'query-string'
-import io from 'socket.io-client'
-import './Chat.css'
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
 
 let socket;
 
-const Chat = ({location}) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const ENDPOINT = "localhost:5000"
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "localhost:5000";
 
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
-    useEffect(() => {
-        const {name, room} = queryString.parse(location.search) 
+    socket = io(ENDPOINT);
 
-        socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
 
-        setName(name);
-        setRoom(room);
+    socket.emit("join", { name, room }, () => {});
+  }, [ENDPOINT, location.search]);
 
-        socket.emit('join', { name, room }, () => {
-        
-        });
-    }, [ENDPOINT, location.search]);
+  useEffect(() => {
+    socket.on("message", (message) => {});
+  });
 
-        return () => {
-            socket.emit('disconnect')
+  return () => {
+    socket.emit("disconnect");
 
-            socket.off();
-        }
+    socket.off();
+  };
 
+  return <h1>Chat</h1>;
+};
 
-    return (
-        <h1>Chat</h1>
-    )
-}
-
-export default Chat
+export default Chat;
